c93e0c1ef7e0eb6655a53924578f1da3d091ee5c
SeongKukLee <lskook99@naver.com>
2024-04-24 12:06:16 +0900

    Add: SmartFactory Asset추가

--- SmartFactory-main/Assets/Player.cs
+++ SmartFactory-main/Assets/Player.cs
@@ -19,22 +19,22 @@ public class Player : MonoBehaviour
         float h = Input.GetAxis("Horizontal"); // -1~1
         float v = Input.GetAxis("Vertical");
 
-        //transform.Translate(Vector3.right * Time.deltaTime * speed);
+        // transform.Translate(Vector3.right * Time.deltaTime * speed);
         // Vector3 direction = (Vector3.right * h) + (Vector3.forward * v); // (1, 0, 0) * -1 = (-1, 0, 0)
                             // x(좌우), y(앞뒤), z(위아래)
         Vector3 direction = new Vector3 (h, 0, v);
         
         transform.position += direction * Time.deltaTime * speed;
         
-        print("Space Button Pressed!");
-        GameObject obj = Instantiate(sphere);
-        float randX = UnityEngine.Random.Range(0.0f, 1.0f);
-        float randY = UnityEngine.Random.Range(0.0f, 1.0f);
-        float randZ = UnityEngine.Random.Range(0.0f, 1.0f);
-        obj.transform.position = new Vector3(randX, randY, randZ);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            print("Space Button Pressed!");
+            GameObject obj = Instantiate(sphere);
+            float randX = UnityEngine.Random.Range(0.0f, 1.0f);
+            float randY = UnityEngine.Random.Range(0.0f, 1.0f);
+            float randZ = UnityEngine.Random.Range(0.0f, 1.0f);
+            obj.transform.position = new Vector3(randX, randY, randZ);
 
         }
     }
